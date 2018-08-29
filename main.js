73caada61ffed38f17d7a09819462db10c5e946b
swj04204 <40710961+swj04204@users.noreply.github.com>
2018-08-29 21:37:20 +0900

    web server
    board 페이지에서 문제 만들고 저장하는 부분 추가했고
    저장한 파일 불러와서 리스트를 만들어 board에 문제이름을 출력하는 것까지 확인했습니다
    + ) 후에 문제이름을 문제번호로 바꾸고, 그 문제번호를 누를시에 해당 문제에 대한 설명이 나오는 페이지를 만들면 될 것 같습니다

--- main.js
+++ main.js
@@ -152,39 +152,111 @@ var app = http.createServer(function(request, response){
     //response.writeHead(302, {Location : `/`});
     //response.end(html);
   } else if(pathname === '/board'){
-    var html = template.HTML(`
-    #menuwrap{
-      //width : auto;
-      border-top : 3px solid black;
-      border-bottom : 3px solid black;
-      padding : 5px;
-    }
-    #boardwrap{
-      //display : grid;
-      //grid-template : auto / 140px auto;
-      //grid-gap : 3px;
-    }`,
-    `<div id="menuwrap">
-        <div id="menu" style="text-align:left; font-weight:bold;"><a href="/board">board</a></div>
-    </div>`,
-    `
-    <div id="mainwrap">
-      <div>
-      여기에 만들어놓은 문제들이 차례대로 보여지도록 코드를 짜야겠지요
-        <h3 style="padding-left:5px;">This is board page</h3>
-        <input type="button" onclick="location.href='/create';" value="create" />
-      </div>
-    </div>
-    `
-  );
-    response.writeHead(300);
-    response.end(html);
-  } else if(pathname==='/create'){
+    fs.readdir('./problem', function(error,filelist){
+      console.log(filelist);
+        if(filelist != ""){
+            var list = template.problem_list(filelist);
+            console.log("success");
+          }
+        else {
+          var list = '<h3 style="padding-left:5px;">This is board page</h3>';
+          console.log("Fail");
+        }
 
+        var html = template.HTML(`
+        #menuwrap{
+          //width : auto;
+          border-top : 3px solid black;
+          border-bottom : 3px solid black;
+          padding : 5px;
+        }
+        #boardwrap{
+          //display : grid;
+          //grid-template : auto / 140px auto;
+          //grid-gap : 3px;
+        }`,
+        `<div id="menuwrap">
+            <div id="menu" style="text-align:left; font-weight:bold;">
+              <a href="/board">board</a>
+            </div>
+        </div>`,
+        `
+        <div id="mainwrap">
+          <div>
+            ${list}
+            <input type="button" onclick="window.location.href='/create';" value="create" />
+          </div>
+        </div>
+        `
+      );
+      response.writeHead(300);
+      response.end(html);
+    })
+  } else if(pathname === '/board/'){
+    // 현재 이부분에 board/{문제번호} 주소인 경우 그 문제에 해당하는 정보를 출력하는 페이지를 만들면 될 것 같습니다
 
+    console.log('success');
 
     response.writeHead(300);
     response.end();
+  } else if(pathname==='/create'){
+      var html = template.HTML(`
+        #writeboard{
+          border : 3px solid;
+          margin-bottom : 200px;
+          min-width : 600px;
+        }
+        `,
+      '',
+      `
+      <div id="writeboard">
+       <h3 style="padding-left : 10px;">Create Problem</h3>
+        <form method="post" action="/create_process" style="padding:10px;">
+          <p>Problem Title</p>
+          <input type="text" name="pb_title" placeholder="Name"></input>
+          <p>Problem Info</p>
+          <textarea name="pb_info" style="min-width:500px; min-height:150px;" placeholder="type information about problem"></textarea>
+          <p>Input Testcase</p>
+          <textarea name="input_test" style="min-width:500px; min-height:100px;" placeholder="Input Testcase"></textarea>
+          <p>Output Testcase</p>
+          <textarea name="output_test" style="min-width:500px; min-height:100px;" placeholder="Output Testcase"></textarea>
+          <p>Input</p>
+          <textarea name="input" style="min-width:500px; min-height:150px;" placeholder="Input"></textarea>
+          <p>Output</p>
+          <textarea name="output" style="min-width:500px; min-height:150px;" placeholder="Output"></textarea>
+          <p>Limit Time<input type="text" name="lim_time" style="margin-left:10px;" placeholder="ms"></input></p>
+          <p>Limit Memory<input type="text" name="lim_mem" style="margin-left:10px;" placeholder="MB"></input></p>
+          <input type="submit" value="create"></input>
+        </form>
+      </div>
+        `);
+
+      response.writeHead(300);
+      response.end(html);
+  } else if(pathname==='/create_process'){
+    /*
+    파일들로 다 저장해봅시다
+    */
+    var body = '';
+        request.on('data', function(data){    // 웹브라우저가 host방식으로 data 전송할때 data가 많으면 그 데이터를 한번에 처리하기에 힘들 수 있기에, 이런 경우에 사용하는 방법인데, data방식은 callback에 들어가있잖아요. 서버에서 조각을 받을때마다 callback함수 호출하기로 했고, 할때마다 data인자를 통해 수신한 정보를 주기로 약속했다.
+            body = body + data;   // callback이 실행될때 마다 인자로 넘겨진 data를 추가하는데. 이게 너무 많아버리면 끊어버리는 코드가 따로 있습니다만 여기에는 포함 안했습니다.
+        });
+        request.on('end', function(){   // 들어올 정보가 없다면 end 다음에 있는 callback이 실행되도록 함.
+            var post = qs.parse(body);    // parse를 통해 객체화 시켜서, post에 우리가 submit으로 제출한 POST의 내용이 담겨있을거다.
+            var description="";
+            var pb_title = post.pb_title;
+            // 문제번호도 자동 생성해서 추가했으면 좋겠네요
+            // 저장하는 방법, 불러오는 방법 정해져야 할 거 같아요
+            description = description + "title=" + pb_title + "&"+ "info=" + post.pb_info + "&" + "input_test=" + post.input_test + "&" + "output_test=" + post.output_test + "&" + "input=" + post.input + "&" + "output=" + post.output;
+            description = description + "&"+ "limit_time=" + post.lim_time + "&" + "limit_memory=" + post.lim_mem;
+            // 이 아래는 파일로 저장하는 법
+            // 가입시에 이미 있는 아이디는 못가입하게 확인하는 것도 필요
+            fs.writeFile(`problem/${pb_title}`, description, 'utf8', function(err){ // 파일 저장이 잘 되면 지금 이 callback함수가 실행되겠죠?
+              // 파일 생성이 끝난후에 이동하는 페이지를 다시 설정해주는 리다이렉션 작업을 실행하는 코드가 아래에 있습니다.
+              response.writeHead(302, {Location : `/board`});    // 302는 리다이렉션 하겠다는 뜻이라고 합니다.
+              response.end();
+            });
+        });
   }
 
 
