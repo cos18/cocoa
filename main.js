9e826e2016b3cdbf54d4fdf2a743cde9708fa52c
sunkyuj <sunkyuj12@gmail.com>
2018-08-31 16:31:52 +0900

    ㅡㄷㄱㅎㄷ

--- main.js
+++ main.js
@@ -380,52 +380,6 @@ var app = http.createServer(function (request, response) {
           cookies = cookie.parse(request.headers.cookie);
         }
 
-<<<<<<< HEAD
-      var que = `insert into Solve (solve_member, solve_problem, result, solve_sec, solve_mem, solve_len, solve_lang) VALUES(${cookies.id}, ${problemNumber}, -1, 0, 0, ${submitCode.length}, 1);`;
-      connection.query(que, function (err, result) {
-        que = `select * FROM Solve where solve_member=${cookies.id} ORDER BY solve_id DESC LIMIT 1;`
-        connection.query(que, function (err, result) { 
-          solve_id = result[0].solve_id;
-          fs.writeFile(`answer_comparing/submit_codes/${solve_id}.c`, submitCode, 'utf8', function (err) {
-            var compile = spawn('gcc', ['-o', `./answer_comparing/convertToExe/${solve_id}.exe`, `./answer_comparing/submit_codes/${solve_id}.c`], {
-              shell: true
-            });
-      
-            compile.stdout.on('data', function (data) {
-              console.log('stdout: ' + data);
-            });
-      
-            compile.stderr.on('data', function (data) {
-              console.log(String(data));
-            });
-      
-            compile.on('exit', function (data) {
-              if (data === 0) {
-                var run = spawn(`./answer_comparing/convertToExe/${solve_id}.exe`, ['<', `./problem/${problemNumber}/input/1.txt`, '>', './tmp.txt'], {
-                  shell: true
-                });
-      
-                run.on('exit', function (output) {
-                  console.log('stdout: ' + output + "!");
-                  fs.readFile(`problem/${problemNumber}/output/1.txt`, 'utf8', function (err, ans) {
-                    console.log("ans:" + ans);
-                    fs.readFile(`./tmp.txt`, 'utf8', function (err, result) {
-                      console.log("result" + result);
-                      if (ans === result) {
-                        que = `UPDATE Solve SET result=0 where solve_id=${solve_id}`;
-                        connection.query(que, function (err, result){
-                          response.writeHead(200);
-                          response.end('Correct!');
-                        });
-                      } else {
-                        que = `UPDATE Solve SET result=2 where solve_id=${solve_id}`;
-                        connection.query(que, function (err, result){
-                          response.writeHead(200);
-                          response.end('NO? SINGO');
-                        });
-                      }
-                      
-=======
         var que = `insert into Solve (solve_member, solve_problem, result, solve_sec, solve_mem, solve_len, solve_lang) VALUES(${cookies.id}, ${problemNumber}, -1, 0, 0, ${submitCode.length}, 1);`;
         connection.query(que, function (err, result) {
           que = `select * FROM Solve where solve_member=${cookies.id} ORDER BY solve_id DESC LIMIT 1;`
@@ -471,7 +425,6 @@ var app = http.createServer(function (request, response) {
                         }
                         
                       });
->>>>>>> ec170016c068612c815aedfa1a6bb61a46e205be
                     });
                   });
                 } else { // 컴파일에러
