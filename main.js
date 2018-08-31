979317984c611af62620bdcecdf4151886a68050
sunkyuj <sunkyuj12@gmail.com>
2018-08-31 14:59:08 +0900

    merge

--- main.js
+++ main.js
@@ -9,6 +9,9 @@ var template = require('./lib/template.js');
 var func = require('./lib/function.js');
 var mysql_con = require('./db/db_con.js')();
 var cookie = require('cookie');
+var cp = require('child_process');
+var spawn = require('child_process').spawn;
+var pt = require('platform-tools');
 
 var connection = mysql_con.init();
 var dbcon_sync = mysql_con.init_sync();
@@ -356,6 +359,61 @@ var app = http.createServer(function (request, response) {
         response.end("잘못된 접근입니다.");
       }
     });
+  } else if(pathname === '/submit_code') {
+    var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function (data) {
+            var post = qs.parse(body);
+            var problemNumber = post.problemNumber;
+            var submitCode = post.submitCode;
+            fs.writeFile(`answer_comparing/submit_codes/${problemNumber}.c`, submitCode, 'utf8', function (err) {
+                response.writeHead(200);
+                response.end('Wait for grading...');
+            });
+
+            var compile = spawn('gcc', ['-o', `./answer_comparing/convertToExe/${problemNumber}.exe`, `./answer_comparing/submit_codes/${problemNumber}.c`], {
+                shell: true
+            });
+
+            compile.stdout.on('data', function (data) {
+                console.log('stdout: ' + data);
+                
+            });
+
+            compile.stderr.on('data', function (data) {
+                console.log(String(data));
+            });
+
+            compile.on('exit', function (data) {
+                if (data === 0) {
+                    var run = spawn(`./answer_comparing/convertToExe/${problemNumber}.exe`, ['<', `./problem/${problemNumber}/input/1.txt`, '>', './tmp.txt'], {
+                        shell: true
+                    });
+
+                    run.on('exit', function (output) {
+                        console.log('stdout: ' + output+"!");
+                        fs.readFile(`problem/${problemNumber}/output/1.txt`, 'utf8', function (err, ans) {
+                          console.log(ans);
+                          fs.readFile(`./tmp.txt`, 'utf8', function (err, result) {
+                              console.log(result);
+                              if(ans===result){
+                                console.log('CORRECT!');
+                              }
+                              else{
+                                console.log('NO? SINGO');
+                              }
+                          });
+  
+                      });
+                    });
+                }
+            });
+  
+
+        });
+
   }
 });
 
--- main_sk.js
+++ main_sk.js
@@ -4,15 +4,8 @@ var url = require('url');
 var qs = require('querystring');
 var pt = require('platform-tools');
 var cp = require('child_process');
-var shell = require('shelljs');
 var spawn = require('child_process').spawn;
-const {
-    c,
-    cpp,
-    node,
-    python,
-    java
-} = require('compile-run');
+
 
 var app = http.createServer(function (request, response) {
     var _url = request.url;
@@ -34,13 +27,6 @@ var app = http.createServer(function (request, response) {
                 response.end('Wait for grading...');
             });
 
-            /*
-                  var run = spawn('./answer_comparing/convertCfiletoExe/a.exe', []);//됨
-                  run.stdout.on('data', function (output) {
-                      console.log(String(output));
-                  });
-            */
-
             var compile = spawn('gcc', ['-o', `./answer_comparing/convertToExe/${problemNumber}.exe`, `./answer_comparing/submit_codes/${problemNumber}.c`], {
                 shell: true
             });
@@ -59,14 +45,7 @@ var app = http.createServer(function (request, response) {
                     var run = spawn(`./answer_comparing/convertToExe/${problemNumber}.exe`, ['<', `./problem/${problemNumber}/input/1.txt`, '>', './tmp.txt'], {
                         shell: true
                     });
-/*
-                    run.stdout.on('data', function (output) {
-                        console.log(String(output));
-                    });
-                    run.stderr.on('data', function (output) {
-                        console.log(String(output));
-                    });
-*/
+
                     run.on('exit', function (output) {
                         console.log('stdout: ' + output+"!");
                         fs.readFile(`problem/${problemNumber}/output/1.txt`, 'utf8', function (err, ans) {
@@ -87,45 +66,7 @@ var app = http.createServer(function (request, response) {
 
                 }
             });
-            /*
-            	fs.readFile(`problem/${problemNumber}/output/1.txt`,'utf8',function(err,description){
-            		console.log(description);
-            	});
-            */
-            /*
-            	fs.readFile(`./tmp.txt`,'utf8',function(err,description){
-                            console.log(description);
-                    });
-            */
-            //	console.log(compile);
-
-            //	var compile2 = spawn(`./answer_comparing/convertToExe/${problemNumber}.exe`,['<',`./problem/${problemNumber}/input/1.txt`,'>','./tmp.txt']);	
-            //	console.log(compile2);
-
-            /*
-                  var compile = spawn('gcc', ['1000.c']);
-                  compile.stdout.on('data', function (data) {
-                    console.log('stdout: ' + data);
-                  });
-                  compile.stderr.on('data', function (data) {
-                    console.log(String(data));
-                  });
-                  compile.on('close', function (data) {
-                      if (data === 0) {
-                          var run = spawn('./a.exe', []);
-                          run.stdout.on('data', function (output) {
-                              console.log(String(output));
-                          });
-                          run.stderr.on('data', function (output) {
-                              console.log(String(output));
-                          });
-                          run.on('close', function (output) {
-                              console.log('stdout: ' + output);
-                          });
-                      }
-                  });
-            */
-
+  
 
         });
     } else {
