4ef392bef0cd6a8e1ed3d5fdd81dce5aa1de2e7c
Sungwoo Park <codest99@gmail.com>
2018-11-29 14:48:28 +0900

    Applying Semantic

--- routes/mypage.js
+++ routes/mypage.js
@@ -12,7 +12,23 @@ router.get('/', function(request, response){
     if (auth.isOwner(request, response)){
       // 이부분을 수정하던, 템플릿을 수정하던 해서 왼쪽에 세부 메뉴를 띄우고 가운데 창에 다른 정보 표시하기
       var html = template.HTML(`
-      <div class=wrapper>
+            <div class="ui secondary vertical pointing menu">
+              <a class="item active">프로필</a>
+              <a class="item">채점 결과</a>
+              <a class="item">문제 추천</a>
+              <a class="item">알림</a>
+              <a class="item">개인정보 수정</a>
+            </div>
+          <div class="ui segment">
+            This is an stretched grid column. This segment will always match the tab height
+          </div>
+      `,
+      template.topbar(request, response)
+      );
+      response.send(html);
+    }
+    /*
+    <div class=wrapper>
         <aside id=side_menu>
           <p><a href="/mypage/login_update">개인정보 수정</a></p>
           <p><a href="/mypage/result">채점결과</a></p>
@@ -21,11 +37,7 @@ router.get('/', function(request, response){
         </aside>
         <div id=con></kklddiv>
       </div>
-      `,
-      template.topbar(request, response)
-      );
-      response.send(html);
-    }
+    */
     else {
       console.log("login error!");
       response.writeHead(302, {
@@ -35,5 +47,15 @@ router.get('/', function(request, response){
     }
 })
 
-router.post('/priv')
+router.post('/result', function(request, response){
+  if(auth.isOwner(request, response)){
+    
+  } else {
+    console.log("login error!");
+      response.writeHead(302, {
+        Location: `/login?error=nologin`
+      });
+      response.end();
+  }
+})
 module.exports = router;
