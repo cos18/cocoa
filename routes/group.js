a18e179c8ed199be976ffcb64bb033ca886fbb2e
Sungwoo Park <codest99@gmail.com>
2019-01-20 17:13:37 +0900

    add function 'send group request'

--- routes/group.js
+++ routes/group.js
@@ -30,8 +30,14 @@ router.get('/', function (request, response) {
 router.get('/myGroup', function (request, response) {
   var myGroupPage;
   if (auth.isOwner(request, response)) {
-    var html = template.group('내 그룹', '내가 속한 그룹', template.topbar(request, response), "Group"); //'내가 속한 그룹' 항목에 그룹 목록이 들어가야함
-    response.send(html);
+    var stmt = `select * from Groups
+    where group_id = (select attend_group from GroupAttend where attend_member = ${auth.getMemberId(request, response)})`;
+    connection.query(stmt, function (err, result) {
+      console.log(result);
+      groupListView = template.group_list(result, request, response);
+      var html = template.group('내 그룹', '내가 속한 그룹', template.topbar(request, response), "Group"); //'내가 속한 그룹' 항목에 그룹 목록이 들어가야함
+      response.send(html);
+    });
   } else {
     console.log("login error!");
     response.writeHead(302, {
@@ -80,15 +86,15 @@ router.get('/createGroup', function (request, response) {
 router.post('/createGroupProcess', function (request, response) {
   let post = request.body;
   console.log(post);
-  let on = (post.groupopen==="on")?1:0;
+  let on = (post.groupopen === "on") ? 1 : 0;
   console.log(on);
   let que = `INSERT INTO Groups (group_name, group_admin, group_open) VALUES("${post.groupname}", ${auth.getMemberId(request, response)}, ${on});`;
   connection.query(que, function (err, result) {
     que = `SELECT group_id from Groups ORDER BY group_id DESC LIMIT 1;`;
-    connection.query(que, function (err, result){
+    connection.query(que, function (err, result) {
       console.log(result[0]);
       que = `INSERT INTO GroupAttend (attend_status, attend_group, attend_member) VALUES(2, ${result[0].group_id}, ${auth.getMemberId(request, response)});`;
-      connection.query(que, function (err, result){
+      connection.query(que, function (err, result) {
         console.log("no3");
         response.redirect(`/group`);
       });
@@ -96,82 +102,87 @@ router.post('/createGroupProcess', function (request, response) {
   });
 });
 
+
+// 그룹 신청 확인 페이지
+router.get('/attend/:group_id', function (request, response) {
+  var group_id = request.params.group_id;
+  que = `INSERT INTO GroupAttend (attend_status, attend_group, attend_member) VALUES(0, ${group_id}, ${auth.getMemberId(request, response)});`;
+  connection.query(que, function (err, result) {
+    console.log("no3");
+    response.redirect(`/group`);
+  });
+});
+
 /* 세부 그룹 관련*/
 // 세부 그룹 문제 페이지
-router.get('/problemlist/:group_id', function(request, response){
+router.get('/problemlist/:group_id', function (request, response) {
   //console.log(request.params.group_id);
   var group_id = request.params.group_id;
   if (group_id === undefined)
-      response.status(404).send('잘못된 접근입니다');
-  else{
-    response.send(template.part_group("문제", group_id,"문제페이지 입니다.",template.topbar(request, response), "Group"));
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("문제", group_id, "문제페이지 입니다.", template.topbar(request, response), "Group"));
   }
-  
-  });
-
-// 세부 그룹 Q&A 페이지
-router.get('/qna/:group_id', function(request, response){
-  //console.log(request.params.group_id);
-  var group_id = request.params.group_id;
-  if (group_id === undefined)
-      response.status(404).send('잘못된 접근입니다');
-  else{
-    response.send(template.part_group("그룹 Q&A", group_id,"그룹 Q&A페이지 입니다.",template.topbar(request, response), "Group"));
-  }
-  
-  });
-
-  // 그룹 신청 확인 페이지
-router.get('/attend/:group_id', function (request, response) {
 
 });
 
-
-  // 세부 그룹 코드리뷰확인 페이지
-  router.get('/review/:group_id', function(request, response){
-    //console.log(request.params.group_id);
-    var group_id = request.params.group_id;
-    if (group_id === undefined)
-        response.status(404).send('잘못된 접근입니다');
-    else{
-      response.send(template.part_group("코드리뷰확인", group_id,"코드리뷰 확인페이지 입니다.",template.topbar(request, response), "Group"));
-    }
-    
-    });
-
-// 세부 그룹 강의 페이지
-router.get('/lecture/:group_id', function(request, response){
+// 세부 그룹 Q&A 페이지
+router.get('/qna/:group_id', function (request, response) {
   //console.log(request.params.group_id);
   var group_id = request.params.group_id;
   if (group_id === undefined)
-      response.status(404).send('잘못된 접근입니다');
-  else{
-    response.send(template.part_group("그룹 강의", group_id,"그룹 강의페이지 입니다.",template.topbar(request, response), "Group"));
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("그룹 Q&A", group_id, "그룹 Q&A페이지 입니다.", template.topbar(request, response), "Group"));
   }
-  
-  });
 
-  // 세부 그룹 관리 페이지
-  router.get('/admin/:group_id', function(request, response){
-    //console.log(request.params.group_id);
-    var group_id = request.params.group_id;
-    if (group_id === undefined)
-        response.status(404).send('잘못된 접근입니다');
-    else{
-      response.send(template.part_group("관리", group_id,"관리 페이지 입니다.",template.topbar(request, response), "Group"));
-    }
-    
-    });
+});
 
-  // 세부 그룹 메인 페이지
-  router.get('/:group_id', function(request, response){
-    //console.log(request.params.group_id);
-    var group_id = request.params.group_id;
-    if (group_id === undefined)
-        response.status(404).send('잘못된 접근입니다');
-    else{
-      response.send(template.part_group("메인", group_id,"메인페이지 입니다.",template.topbar(request, response), "Group"));
-    }
-    
-  });
+// 세부 그룹 코드리뷰확인 페이지
+router.get('/review/:group_id', function (request, response) {
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("코드리뷰확인", group_id, "코드리뷰 확인페이지 입니다.", template.topbar(request, response), "Group"));
+  }
+
+});
+
+// 세부 그룹 강의 페이지
+router.get('/lecture/:group_id', function (request, response) {
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("그룹 강의", group_id, "그룹 강의페이지 입니다.", template.topbar(request, response), "Group"));
+  }
+
+});
+
+// 세부 그룹 관리 페이지
+router.get('/admin/:group_id', function (request, response) {
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("관리", group_id, "관리 페이지 입니다.", template.topbar(request, response), "Group"));
+  }
+
+});
+
+// 세부 그룹 메인 페이지
+router.get('/:group_id', function (request, response) {
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+    response.status(404).send('잘못된 접근입니다');
+  else {
+    response.send(template.part_group("메인", group_id, "메인페이지 입니다.", template.topbar(request, response), "Group"));
+  }
+
+});
 module.exports = router;
