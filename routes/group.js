d9096e255cf8e3628ccaeea343ec4c9c55842665
Kyeong Ryeol Kim <swj04204@gmail.com>
2019-01-20 15:33:19 +0900

    세부그룹 페이지

--- routes/group.js
+++ routes/group.js
@@ -11,7 +11,7 @@ var connection = mysql_con.init();
 router.get('/', function(request, response){
     var everyGroupPage;
     if(auth.isOwner(request, response)){
-        var html = template.group('전체 그룹', '전체 그룹 페이지', template.topbar(request, response, "Group")); //'전체 그룹 페이지' 항목에 그룹 목록이 들어가야함
+        var html = template.group('전체 그룹', '전체 그룹 페이지', template.topbar(request, response), "Group"); //'전체 그룹 페이지' 항목에 그룹 목록이 들어가야함
         response.send(html);
       } else {
         console.log("login error!");
@@ -26,7 +26,7 @@ router.get('/', function(request, response){
 router.get('/myGroup', function(request, response){
     var myGroupPage;
     if(auth.isOwner(request, response)){
-        var html = template.group('내 그룹', '내가 속한 그룹', template.topbar(request, response, "Group")); //'내가 속한 그룹' 항목에 그룹 목록이 들어가야함
+        var html = template.group('내 그룹', '내가 속한 그룹', template.topbar(request, response), "Group"); //'내가 속한 그룹' 항목에 그룹 목록이 들어가야함
         response.send(html);
       } else {
         console.log("login error!");
@@ -67,7 +67,7 @@ router.get('/createGroup', function(request, response){
     </form>  		
     `;
     if(auth.isOwner(request, response)){
-        var html = template.group('그룹 만들기',createGroupTemplate, template.topbar(request, response, "Group")); //'그룹 만들기 양식' 항목에 만드는 양식이 들어가야함
+        var html = template.group('그룹 만들기',createGroupTemplate, template.topbar(request, response), "Group"); //'그룹 만들기 양식' 항목에 만드는 양식이 들어가야함
         response.send(html);
       } else {
         console.log("login error!");
@@ -91,5 +91,76 @@ router.post('/group_process', function (request, response) {
   */
 });
 
+/* 세부 그룹 관련*/
+// 세부 그룹 문제 페이지
+router.get('/problemlist/:group_id', function(request, response){
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+      response.status(404).send('잘못된 접근입니다');
+  else{
+    response.send(template.part_group("문제", group_id,"문제페이지 입니다.",template.topbar(request, response), "Group"));
+  }
+  
+  });
 
+// 세부 그룹 Q&A 페이지
+router.get('/qna/:group_id', function(request, response){
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+      response.status(404).send('잘못된 접근입니다');
+  else{
+    response.send(template.part_group("그룹 Q&A", group_id,"그룹 Q&A페이지 입니다.",template.topbar(request, response), "Group"));
+  }
+  
+  });
+
+  // 세부 그룹 코드리뷰확인 페이지
+  router.get('/review/:group_id', function(request, response){
+    //console.log(request.params.group_id);
+    var group_id = request.params.group_id;
+    if (group_id === undefined)
+        response.status(404).send('잘못된 접근입니다');
+    else{
+      response.send(template.part_group("코드리뷰확인", group_id,"코드리뷰 확인페이지 입니다.",template.topbar(request, response), "Group"));
+    }
+    
+    });
+
+// 세부 그룹 강의 페이지
+router.get('/lecture/:group_id', function(request, response){
+  //console.log(request.params.group_id);
+  var group_id = request.params.group_id;
+  if (group_id === undefined)
+      response.status(404).send('잘못된 접근입니다');
+  else{
+    response.send(template.part_group("그룹 강의", group_id,"그룹 강의페이지 입니다.",template.topbar(request, response), "Group"));
+  }
+  
+  });
+
+  // 세부 그룹 관리 페이지
+  router.get('/admin/:group_id', function(request, response){
+    //console.log(request.params.group_id);
+    var group_id = request.params.group_id;
+    if (group_id === undefined)
+        response.status(404).send('잘못된 접근입니다');
+    else{
+      response.send(template.part_group("관리", group_id,"관리 페이지 입니다.",template.topbar(request, response), "Group"));
+    }
+    
+    });
+
+  // 세부 그룹 메인 페이지
+  router.get('/:group_id', function(request, response){
+    //console.log(request.params.group_id);
+    var group_id = request.params.group_id;
+    if (group_id === undefined)
+        response.status(404).send('잘못된 접근입니다');
+    else{
+      response.send(template.part_group("메인", group_id,"메인페이지 입니다.",template.topbar(request, response), "Group"));
+    }
+    
+  });
 module.exports = router;
