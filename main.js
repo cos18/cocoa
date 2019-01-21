5d5a63b33d9c7318bc6066ddd200d52e1e5d775f
Sungwoo Park <codest99@gmail.com>
2019-01-21 17:16:17 +0900

    fixing main page

--- main.js
+++ main.js
@@ -44,15 +44,97 @@ app.use('/group',groupRouter);
 
 // 메인페이지
 app.get('/', function (request, response) {
-  if (!auth.isOwner(request, response)) { // undefined면 home임.
-    var html = template.HTML(
-      `<h3>This is main page</h3>`, template.topbar(request, response));
-    response.send(html);
-  } else {
-    var html = template.HTML(
-      `<h3>Login success! Welcome!</h3>`, template.topbar(request, response));
-    response.send(html);
-  }
+  let main = `
+  <div class="ui vertical masthead center aligned segment">
+    <div class="ui text container" style="margin-top:140px; margin-bottom:100px;">
+      <h1 class="ui header" style="font-size: 55px">
+        COmputer COding Artist
+      </h1>
+      <h3>이곳에서 여러분의 꿈을 시작해보세요!</h2>
+      <br>
+      <div class="ui big primary button">시작하기 <i class="right arrow icon"></i></div>
+    </div>
+  </div>
+  <div class="coblock ui vertical stripe segment">
+    <div class="ui middle aligned stackable grid container">
+      <div class="row">
+        <div class="eight wide column">
+          <h2 class="ui header">시작이 반인데, 어디서 시작하지?</h2>
+          <p>기존 저지 사이트(Judge Site - 프로그래밍 문제를 풀 수 있는 사이트)에서는 입문자들을 위한 문제, 기능들이 부족합니다. 주로 실력자들도 어려워하는 올림피아드나 대회 문제들이 수록되어있기 때문이죠.</p>
+          <h2 class="ui header">여러분의 첫 코딩 경험을 도와드립니다</h2>
+          <p>기초를 다질 수 있는 문제가 많이 준비될 예정이고, 학생별 맞춤 문제 추천 시스템을 도입할 예정입니다. 첫 코딩 경험은 코코아에서!</p>
+        </div>
+        <div class="six wide right floated column">
+          <img src="image/main_screen.png" class="ui large image">
+        </div>
+      </div>
+      <div class="row">
+        <div class="center aligned column">
+          <a class="ui large button">문제 보러가기</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui vertical stripe quote segment">
+    <div class="ui equal width stackable internally celled grid">
+      <div class="center aligned row">
+        <div class="column">
+          <h3>"What a Company"</h3>
+          <p>That is what they all say about us</p>
+        </div>
+        <div class="column">
+          <h3>"I shouldn't have gone with their competitor."</h3>
+          <p>
+            <img src="assets/images/avatar/nan.jpg" class="ui avatar image"> <b>Nan</b> Chief Fun Officer Acme Toys
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="ui vertical stripe segment">
+    <div class="ui text container">
+      <h3 class="ui header">Breaking The Grid, Grabs Your Attention</h3>
+      <p>Instead of focusing on content creation and hard work, we have learned how to master the art of doing nothing by providing massive amounts of whitespace and generic content that can seem massive, monolithic and worth your attention.</p>
+      <a class="ui large button">Read More</a>
+      <h4 class="ui horizontal header divider">
+        <a href="#">Case Studies</a>
+      </h4>
+      <h3 class="ui header">Did We Tell You About Our Bananas?</h3>
+      <p>Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but its really true. It took years of gene splicing and combinatory DNA research, but our bananas can really dance.</p>
+      <a class="ui large button">I'm Still Quite Interested</a>
+    </div>
+  </div>
+  <div class="ui inverted vertical footer segment">
+    <div class="ui container">
+      <div class="ui stackable inverted divided equal height stackable grid">
+        <div class="three wide column">
+          <h4 class="ui inverted header">About</h4>
+          <div class="ui inverted link list">
+            <a href="#" class="item">Sitemap</a>
+            <a href="#" class="item">Contact Us</a>
+            <a href="#" class="item">Religious Ceremonies</a>
+            <a href="#" class="item">Gazebo Plans</a>
+          </div>
+        </div>
+        <div class="three wide column">
+          <h4 class="ui inverted header">Services</h4>
+          <div class="ui inverted link list">
+            <a href="#" class="item">Banana Pre-Order</a>
+            <a href="#" class="item">DNA FAQ</a>
+            <a href="#" class="item">How To Access</a>
+            <a href="#" class="item">Favorite X-Men</a>
+          </div>
+        </div>
+        <div class="seven wide column">
+          <h4 class="ui inverted header">Footer Header</h4>
+          <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+  const html = template.HTML(main, template.topbar(request, response));
+  response.send(html);
 })
 
 // 채점결과 페이지 (안쓸 예정)
